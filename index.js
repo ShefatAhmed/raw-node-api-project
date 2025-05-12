615128bbcb56d82469cec74f4d25fc682ef5036a
unknown <shefata69@gmail.com>
2025-05-12 16:33:24 +0600

    update

--- index.js
+++ index.js
@@ -14,14 +14,18 @@ const app = {};
 // data.read("test", "newFile", (err, data) => {
 //   console.log(err, data);
 // });
-data.update(
-  "test",
-  "newFile",
-  { name: "bangladesh", language: "english" },
-  (err) => {
-    console.log("Error", err);
-  }
-);
+// data.update(
+//   "test",
+//   "newFile",
+//   { name: "bangladesh", language: "english" },
+//   (err) => {
+//     console.log("Error", err);
+//   }
+// );
+
+data.delete("test", "newFile", (err) => {
+  console.log("Error", err);
+});
 app.createServer = () => {
   const server = http.createServer(app.handleReqRes);
   server.listen(enviroment.port, () => {
