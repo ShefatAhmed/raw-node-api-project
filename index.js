842fd620c77610dc422f8759b40990e158755266
unknown <shefata69@gmail.com>
2025-05-12 13:06:31 +0600

    create data

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const http = require("http");
 const { handleReqRes } = require("./helpers/handleReqRes");
 const enviroment = require("./helpers/enviroments");
+const data = require("./lib/data");
 const app = {};
+data.create(
+  "test",
+  "newFile",
+  { name: "bangladesh", language: "bangla" },
+  (err) => {
+    console.log("Error", err);
+  }
+);
 app.createServer = () => {
   const server = http.createServer(app.handleReqRes);
   server.listen(enviroment.port, () => {
